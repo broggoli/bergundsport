1f6774d69b5cfe3f39f7573520490979dc32d366
broggoli <broggoli.nb@gmail.com>
2019-08-30 18:50:04 +0200

    Added the basic structure of the PWA.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: "", loadChildren: "./pages/menu/menu.module#MenuPageModule" },
+  { path: 'material', loadChildren: './pages/material/material.module#MaterialPageModule' }
 ];
 
 @NgModule({
@@ -12,4 +12,4 @@ const routes: Routes = [
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
